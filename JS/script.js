44d97864c34a18d6a756c484c89fa78ec0613ac0
NailsonChagas <chagasnailson@gmail.com>
2021-10-24 16:31:15 -0300

    first commit

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,71 @@
-const cartas = document.querySelectorAll('.carta');
-let existeCartasViradas = false;
-let primeiraCarta, segundaCarta;
+
+const cards = document.querySelectorAll('.carta');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 let lockBoard = false;
 
-//virar carta
-function flipCard(){
-    if(lockBoard) return undefined;
-    if(this === primeiraCarta) return undefined;
+//função para virar carta
+function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
 
     this.classList.add('virar');
-    if(!existeCartasViradas){
-        existeCartasViradas = true;
-        primeiraCarta = this;
-        return undefined;
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
     }
 
-    segundaCarta = this;
-    existeCartasViradas = false;
+    secondCard = this;
+    hasFlippedCard = false;
     checkForMatch();
 }
 
-//checa se cartas selecionadas são iguais
+//função que checa se as cartas são iguais
+function checkForMatch() {
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+//função que desabilita as cartas
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+//funcão que desvira as cartas
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('virar');
+        secondCard.classList.remove('virar');
+
+        resetBoard();
+    }, 1500);
+}
+
+//função que reseta o tabuleiro
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+//função que embaralha as cartas
+(function shuffle() {
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPosition;
+    })
+})();
+
+//adiciona evento de clique na carta
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+});
